6772a31a2827552403523b10ebd5d17c488ea6b2
Piotr Ryczek <p.ryczek@binarapps.com>
2020-05-14 20:40:50 +0200

    a4

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // Importing models
 import 'models/country';
 
-const connectionUrl = process.env.MONGO_CONNECTION;
+const connectionUrl = `${process.env.MONGO_CONNECTION}/${process.env.MONGO_DATABASE}`;
 
 mongoose.connect(connectionUrl, {
   useNewUrlParser: true,
