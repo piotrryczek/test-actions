e360891070d024de7aee9137d22fce43da65b60f
Piotr Ryczek <p.ryczek@binarapps.com>
2020-05-14 23:43:27 +0200

    As Issue

--- src/models/___tests__/sample.test.ts
+++ src/models/___tests__/sample.test.ts
@@ -14,6 +14,7 @@ import Country from 'models/country';
 
 describe('Sample describe', () => {
   before(async () => {
+    console.log('Connection URL:', connectionUrl);
     return new Promise((resolve, reject) => {
       mongoose.connect(connectionUrl, connectionSettings).then(() => {
         console.log('Connected');
