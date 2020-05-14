afc6192c8c8bc3c69ad89b4ddad4e86457fc20a6
Piotr Ryczek <p.ryczek@binarapps.com>
2020-05-14 23:11:12 +0200

    z2

--- src/models/___tests__/sample.test.ts
+++ src/models/___tests__/sample.test.ts
@@ -1,15 +1,25 @@
 import { expect } from 'chai';
 
 import 'config/env';
-import * as http from 'http';
-import supertest from 'supertest';
 
-import app from 'app';
+import mongoose from 'mongoose';
+// import * as http from 'http';
+// import supertest from 'supertest';
+
+// import app from 'app';
+import { connectionUrl, connectionSettings } from 'config/db';
 import Country from 'models/country';
 
-supertest(http.createServer(app.callback()));
+// supertest(http.createServer(app.callback()));
 
 describe('First test', () => {
+
+  before(async () => {
+    console.log('Przed połączeniem z DB');
+    await mongoose.connect(connectionUrl, connectionSettings);
+    console.log('Po połączeniu z DB')
+  });
+
   it('should return true', async () => {
     console.log('Zaczynamy test');
     expect(1).to.equal(1);
@@ -21,36 +31,3 @@ describe('First test', () => {
     expect(country.name).to.equal('Polska');
   });
 });
-
-
-// // import mongoose from 'mongoose';
-
-// 
-
-// // import { connectionUrl, connectionSettings } from 'config/db';
-
-// 
-
-
-// const sleep = time => new Promise(resolve => setTimeout(() => { resolve(); }, time * 1000));
-
-// describe('Sample testing', () => {
-//   beforeAll(async (done) => {
-//     console.log('Mongoose przed połączeniem');
-
-//     await sleep(2);
-
-//     console.log('Mongoose po połączeniu');
-
-//     done();
-//     // mongoose.connect(connectionUrl, connectionSettings).then(() => {
-//     //   console.log('Mongoose po połączeniu');
-//     //   done();
-//     // }).catch(error => console.log('blad', error));
-
-//   });
-
-//   // afterAll((done) => {
-//   //   mongoose.disconnect(done);
-//   // });
-// });
