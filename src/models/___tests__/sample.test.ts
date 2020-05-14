3068597ab729575bc70eced98ad238c77f90dbc1
Piotr Ryczek <p.ryczek@binarapps.com>
2020-05-14 23:05:37 +0200

    z1

--- src/models/___tests__/sample.test.ts
+++ src/models/___tests__/sample.test.ts
@@ -1,48 +1,56 @@
+import { expect } from 'chai';
+
 import 'config/env';
 import * as http from 'http';
 import supertest from 'supertest';
-// import mongoose from 'mongoose';
 
 import app from 'app';
 import Country from 'models/country';
-// import { connectionUrl, connectionSettings } from 'config/db';
 
 supertest(http.createServer(app.callback()));
 
-// jest.setTimeout(5000);
-
-const sleep = time => new Promise(resolve => setTimeout(() => { resolve(); }, time * 1000));
-
-describe('Sample testing', () => {
-  beforeAll(async (done) => {
-    console.log('Mongoose przed połączeniem');
-
-    await sleep(2);
-
-    console.log('Mongoose po połączeniu');
-
-    done();
-    // mongoose.connect(connectionUrl, connectionSettings).then(() => {
-    //   console.log('Mongoose po połączeniu');
-    //   done();
-    // }).catch(error => console.log('blad', error));
-    
-  });
-
-  it('Sample group should', async (done) => {
+describe('First test', () => {
+  it('should return true', async () => {
     console.log('Zaczynamy test');
-    expect(1).toEqual(1);
+    expect(1).to.equal(1);
 
     const country = new Country({ name: 'Polska' });
     console.log('Przed zapisem');
     await country.save();
     console.log('Po zapisie');
-    expect(country.name).toEqual('Polska');
-
-    done();
+    expect(country.name).to.equal('Polska');
   });
-
-  // afterAll((done) => {
-  //   mongoose.disconnect(done);
-  // });
 });
+
+
+// // import mongoose from 'mongoose';
+
+// 
+
+// // import { connectionUrl, connectionSettings } from 'config/db';
+
+// 
+
+
+// const sleep = time => new Promise(resolve => setTimeout(() => { resolve(); }, time * 1000));
+
+// describe('Sample testing', () => {
+//   beforeAll(async (done) => {
+//     console.log('Mongoose przed połączeniem');
+
+//     await sleep(2);
+
+//     console.log('Mongoose po połączeniu');
+
+//     done();
+//     // mongoose.connect(connectionUrl, connectionSettings).then(() => {
+//     //   console.log('Mongoose po połączeniu');
+//     //   done();
+//     // }).catch(error => console.log('blad', error));
+
+//   });
+
+//   // afterAll((done) => {
+//   //   mongoose.disconnect(done);
+//   // });
+// });
