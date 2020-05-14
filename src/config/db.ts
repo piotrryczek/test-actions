ccfb21e51ecb451b4d367887722f3889877b4122
Piotr Ryczek <p.ryczek@binarapps.com>
2020-05-14 21:04:13 +0200

    q4

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,6 +5,8 @@ import 'models/country';
 
 const connectionUrl = `${process.env.MONGO_CONNECTION}/${process.env.MONGO_DATABASE}`;
 
+console.log('Adres połączenia', connectionUrl);
+
 mongoose.connect(connectionUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
